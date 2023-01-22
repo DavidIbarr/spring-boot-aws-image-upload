856ba51ce33af941e3672593c5bcdef8e89096d6
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-01-22 14:17:39 -0600

    utilize Amazon s3 storage

--- src/main/java/com/example/awsreactimageupload/config/AmazonConfig.java
+++ src/main/java/com/example/awsreactimageupload/config/AmazonConfig.java
@@ -1,5 +1,15 @@
 package com.example.awsreactimageupload.config;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AmazonConfig {
 
+    @Bean
+    public AmazonS3 s3() {
+        return AmazonS3ClientBuilder.defaultClient();
+    }
 }
