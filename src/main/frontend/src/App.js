6da81a52b42f976fc6ab205011cc68321068f14b
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-01-23 10:24:13 -0600

    connecting to backend

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import {useEffect} from "react";
+
+const UserProfiles = () => {
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile")
+    .then(res => {
+      console.log(res);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return <h1>Hello</h1>;
+};
+function App() {
+  return (
+    <div className="App">
+      <UserProfiles />
+    </div>
+  );
+}
+
+export default App;
